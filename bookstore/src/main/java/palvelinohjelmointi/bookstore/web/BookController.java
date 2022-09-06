791e959a9fa46ad2b3845341c9df2ab71ee46e60
Jukka-Pekka Sirviö <sirviojp@gmail.com>
2022-09-06 20:25:15 +0300

    JPA tehtävä 3, lisätty Create

--- bookstore/src/main/java/palvelinohjelmointi/bookstore/web/BookController.java
+++ bookstore/src/main/java/palvelinohjelmointi/bookstore/web/BookController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import palvelinohjelmointi.bookstore.domain.Book;
 import palvelinohjelmointi.bookstore.domain.BookRepository;
 
 @Controller
@@ -16,4 +18,16 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "index";
     }
+    
+    @RequestMapping(value= {"/add"})
+    public String addBook(Model model) {	
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:index";
+    }    
 }
